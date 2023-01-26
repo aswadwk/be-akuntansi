00f450bf0ab1c9e7e4b257eac339485e36ca8a2e
Hajar Aswad <hajaraswadkom@gmail.com>
2023-01-26 23:23:23 +0700

    refactor: add accessor

--- app/Models/AccountType.php
+++ app/Models/AccountType.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use App\Traits\Uuid;
 use Carbon\Traits\Timestamp;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -13,4 +14,18 @@ class AccountType extends Model
     use HasFactory, Uuid, SoftDeletes, Timestamp;
 
     public $guarded = [];
+
+    public function createdBy()
+    {
+
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+    public function positionNormal(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => $value === 'C' ? 'Credit' : 'Debet',
+            // set: fn($value) => Crypt::encryptString($value),
+        );
+    }
 }
