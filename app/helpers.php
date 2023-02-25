e5ec810862193820008b113a0d695bf9d32e4e1e
Hajar Aswad <hajaraswadkom@gmail.com>
2023-02-25 08:43:38 +0700

    feat: ci cd

--- app/helpers.php
+++ app/helpers.php
@@ -8,21 +8,5 @@ if (!function_exists('sum')) {
     }
 }
 
-// make function USD to Rp with with format Rp 28.000,00
-if (!function_exists('usd_to_rp')) {
-    function usd_to_rp($usd)
-    {
-        // if($usd === null || $usd === 0) {
-        //     return 'Rp 0,00';
-        // }
-
-        if(!is_numeric($usd)) {
-            $usd = 0;
-        }
-
-        return 'Rp ' . number_format($usd * 14000, 2, ',', '.');
-    }
-}
-
 
 ;?>
