14bc13e0c686c35726fd23768311909dae270ea8
Hajar Aswad <hajaraswadkom@gmail.com>
2023-10-04 19:18:15 +0700

    refactor: account

--- database/seeders/AccountSeeder.php
+++ database/seeders/AccountSeeder.php
@@ -18,115 +18,142 @@ class AccountSeeder extends Seeder
     {
         $user = User::all()->first();
 
+        $accountType = AccountType::where('code', '111000')->first();
         Account::create([
             'code'            => '111000',
             'name'            => 'Aktiva Lancar',
-            'account_type_id' => AccountType::where('code', '111000')->first()->id,
+            'account_type_id' => $accountType->id,
+            'position_normal' => $accountType->position_normal,
             'user_id'         => $user->id,
             'created_at'      => now(),
             'updated_at'      => now(),
         ]);
+        $accountType = AccountType::where('code', '111000')->first();
         Account::create([
             'code'            => '111001',
             'name'            => 'Kas Rupiah',
-            'account_type_id' => AccountType::where('code', '111000')->first()->id,
+            'account_type_id' => $accountType->id,
+            'position_normal' => $accountType->position_normal,
             'user_id'         => $user->id,
             'created_at'      => now(),
             'updated_at'      => now(),
         ]);
+        $accountType = AccountType::where('code', '111000')->first();
         Account::create([
             'code'            => '111002',
             'name'            => 'Saldo Tokopedia',
-            'account_type_id' => AccountType::where('code', '111000')->first()->id,
+            'account_type_id' => $accountType->id,
+            'position_normal' => $accountType->position_normal,
             'user_id'         => $user->id,
             'created_at'      => now(),
             'updated_at'      => now(),
         ]);
+        $accountType = AccountType::where('code', '111000')->first();
         Account::create([
             'code'            => '111003',
             'name'            => 'Saldo Flip',
-            'account_type_id' => AccountType::where('code', '111000')->first()->id,
+            'account_type_id' => $accountType->id,
+            'position_normal' => $accountType->position_normal,
             'user_id'         => $user->id,
             'created_at'      => now(),
             'updated_at'      => now(),
         ]);
+        $accountType = AccountType::where('code', '111000')->first();
         Account::create([
             'code'            => '111004',
             'name'            => 'Saldo Link Aja',
-            'account_type_id' => AccountType::where('code', '111000')->first()->id,
+            'account_type_id' => $accountType->id,
+            'position_normal' => $accountType->position_normal,
             'user_id'         => $user->id,
             'created_at'      => now(),
             'updated_at'      => now(),
         ]);
+        $accountType = AccountType::where('code', '111000')->first();
         Account::create([
             'code'            => '111005',
             'name'            => 'Bank BRI',
-            'account_type_id' => AccountType::where('code', '111000')->first()->id,
+            'account_type_id' => $accountType->id,
+            'position_normal' => $accountType->position_normal,
             'user_id'         => $user->id,
             'created_at'      => now(),
             'updated_at'      => now(),
         ]);
+        $accountType = AccountType::where('code', '111000')->first();
         Account::create([
             'code'            => '111006',
             'name'            => 'Piutang Usaha',
-            'account_type_id' => AccountType::where('code', '111000')->first()->id,
+            'account_type_id' => $accountType->id,
+            'position_normal' => $accountType->position_normal,
             'user_id'         => $user->id,
             'created_at'      => now(),
             'updated_at'      => now(),
         ]);
-        //
+        $accountType = AccountType::where('code', '112000')->first();
         Account::create([
             'code'            => '112000',
             'name'            => 'Aktiva Tetap',
-            'account_type_id' => AccountType::where('code', '112000')->first()->id,
+            'account_type_id' => $accountType->id,
+            'position_normal' => $accountType->position_normal,
             'user_id'         => $user->id,
             'created_at'      => now(),
             'updated_at'      => now(),
         ]);
+        $accountType = AccountType::where('code', '112000')->first();
         Account::create([
             'code'            => '112001',
             'name'            => 'Tanah',
-            'account_type_id' => AccountType::where('code', '112000')->first()->id,
+            'account_type_id' => $accountType->id,
+            'position_normal' => $accountType->position_normal,
             'user_id'         => $user->id,
             'created_at'      => now(),
             'updated_at'      => now(),
         ]);
+        $accountType = AccountType::where('code', '112000')->first();
         Account::create([
             'code'            => '112002',
             'name'            => 'Bagunan',
-            'account_type_id' => AccountType::where('code', '112000')->first()->id,
+            'account_type_id' => $accountType->id,
+            'position_normal' => $accountType->position_normal,
             'user_id'         => $user->id,
             'created_at'      => now(),
             'updated_at'      => now(),
         ]);
+        $accountType = AccountType::where('code', '112000')->first();
         Account::create([
             'code'            => '112003',
             'name'            => 'Kendaraan',
-            'account_type_id' => AccountType::where('code', '112000')->first()->id,
+            'account_type_id' => $accountType->id,
+            'position_normal' => $accountType->position_normal,
             'user_id'         => $user->id,
             'created_at'      => now(),
             'updated_at'      => now(),
         ]);
+        $accountType = AccountType::where('code', '112000')->first();
         Account::create([
             'code'            => '112004',
             'name'            => 'Mesin dan Perlatan',
-            'account_type_id' => AccountType::where('code', '112000')->first()->id,
+            'account_type_id' => $accountType->id,
+            'position_normal' => $accountType->position_normal,
             'user_id'         => $user->id,
             'created_at'      => now(),
             'updated_at'      => now(),
         ]);
+        $accountType = AccountType::where('code', '112000')->first();
         Account::create([
             'code'            => '112005',
             'name'            => 'Perabot atau Mebel',
-            'account_type_id' => AccountType::where('code', '112000')->first()->id,
+            'account_type_id' => $accountType->id,
+            'position_normal' => $accountType->position_normal,
             'user_id'         => $user->id,
             'created_at'      => now(),
             'updated_at'      => now(),
         ]);
+        $accountType = AccountType::where('code', '112000')->first();
         Account::create([
             'code'            => '112006',
             'name'            => 'Akumulasi Penyusutan',
-            'account_type_id' => AccountType::where('code', '112000')->first()->id,
+            'account_type_id' => $accountType->id,
+            'position_normal' => $accountType->position_normal,
             'user_id'         => $user->id,
             'created_at'      => now(),
             'updated_at'      => now(),
