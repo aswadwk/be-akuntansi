dd75aa1a4734f70781ecb9c46dcfceb22dbfc8de
Hajar Aswad <hajaraswadkom@gmail.com>
2023-10-12 11:25:13 +0700

    refactor: buku besar

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use App\Traits\Uuid;
 use Carbon\Traits\Timestamp;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -22,5 +23,12 @@ class Account extends Model
     public function createdBy()
     {
         return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+    protected function balance(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => + ($value),
+        );
     }
 }
