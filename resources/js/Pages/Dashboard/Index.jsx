36a8fde1f1687e72c526cc3410cb4d31b2da192c
Hajar Aswad <hajaraswadkom@gmail.com>
2023-11-17 15:05:29 +0700

    feat: setup innertia

---
+++ resources/js/Pages/Dashboard/Index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../../Shared/Layout'
+
+const index = () => {
+    return (
+        <Layout>
+            {/* <Head title="Welcome" /> */}
+            <h1>Welcome</h1>
+            <p>Hello , welcome to your first Inertia app!</p>
+        </Layout>
+    )
+}
+
+export default index
